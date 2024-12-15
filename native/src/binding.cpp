98cbdd8c1b22db5b378afc726a2bbc03b817f0b7
rudrakabir <rudrakabir@gmail.com>
2024-12-15 22:32:50 +0530

    full fukcery

--- native/src/binding.cpp
+++
@@ -1,8 +0,0 @@
-#include <napi.h>
-#include "whisper_node.h"
-
-Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
-    return WhisperContext::Init(env, exports);
-}
-
-NODE_API_MODULE(whisper, InitAll)
